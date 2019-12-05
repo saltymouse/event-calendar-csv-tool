0d9e2b6108db1199db864cdc590d64dc5441d607
saltymouse <7547649+saltymouse@users.noreply.github.com>
2019-12-04 20:34:35 -0800

    fix eslint errors

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  rules: {
+    "no-param-reassign": 0,
+  },
+  env: {
+    browser: true,
+    jest: true
+  }
+};
--- source/scripts/buttons/upload.js
+++ source/scripts/buttons/upload.js
@@ -2,7 +2,7 @@ const inputBox = document.querySelector('#svg');
 const uploadButton = document.querySelector('#upload-file');
 
 // アップロードボタン
-uploadButton.addEventListener('change', function () {
+uploadButton.addEventListener('change', function () { // eslint-disable-line func-names
   const file = this.files[0];
   const reader = new FileReader();
 
